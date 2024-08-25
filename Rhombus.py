9186e4705a57f00058bac0dd28cc03a0884d35e5
boss <75540581+Boss22341231@users.noreply.github.com>
2024-08-25 14:54:42 +0700

    Second commit

--- Rhombus.py
+++ Rhombus.py
@@ -3,17 +3,9 @@ row = int(input("Enter a number for rows : "))
 
 if row > 1:
     for i in range(row):
-        for j in range(row - i - 1):
-            print(" ", end="")
-        for k in range((2 * i) + 1):
-            print("#", end="")
-        print()
-
-    for i in range(row):
-        for j in range(i):
-            print(" ", end="")
-        for k in range(2 * (row - i) - 1):
-            print("#", end="")
-        print()
+        print(" " * (row - i - 1) + "#" * (2 * i + 1))
+    
+    for i in range(row - 2, -1, -1):
+        print(" " * (row - i - 1) + "#" * (2 * i + 1))
 else:
     print("The number must more than 1")
